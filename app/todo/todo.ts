c51d58177739caed512f18d9856b18fb8e160abf
qxe <xie.q@sfeir.com>
2016-01-27 12:01:09 +0100

    bug fix

--- app/todo/todo.ts
+++ app/todo/todo.ts
@@ -1,10 +1,10 @@
 export class Todo {
-	id: number,
+	id: number;
 	item: string;
 	completed: boolean;
 	
 	constructor(item: string) {
-		this.id = new Date().getTime;
+		this.id = new Date().getTime();
 		this.item = item;
 		this.completed = false;
 	}
