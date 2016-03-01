13a872b15d79d0ecf683b40e8be0ae10912b25bf
qxe <xie.q@sfeir.com>
2016-03-01 14:55:12 +0100

    add esc and blur event handler

--- app/todo/todo-list.component.ts
+++ app/todo/todo-list.component.ts
@@ -8,13 +8,15 @@ import {TodoService} from './todo.service';
 		<input [(ngModel)]="newTodo" placeholder='What needs to be done?' (keyup.enter)="addTodo()" autofocus>
 		<button (click)="addTodo()">Add</button>
 		<ul>
-			<li *ngFor="#todo of todos">
+			<li *ngFor="#todo of todos">			
 				<input type='checkbox' [(ngModel)]="todo.completed" (click)="toggleTodo(todo)">
-				<span [class.completed]="todo.completed" [style.display]="isEditing(todo)?'none':'inline'" (dblclick)="onEditTodo(todo)">{{todo.item}}</span>
-				<input [(ngModel)]="todo.item" [style.display]="isEditing(todo)?'inline':'none'" (keyup.enter)="saveEditedTodo(todo)">
+				<span [class.completed]="todo.completed" [style.display]="isEditing(todo)?'none':'inline'" (dblclick)="onEditTodo(todo, todoInput)">{{todo.item}}</span>
+				<input #todoInput [(ngModel)]="todo.item" [style.display]="isEditing(todo)?'inline':'none'" 
+					(keyup.enter)="saveEditedTodo(todo)" (keyup.escape)="onCancelEdit(todo)" (blur)="saveEditedTodo(todo)">
 				<button (click)="deleteTodo(todo)">delete</button>
 			</li>
 		</ul>
+		<p>To edit the to do item: double click it.</p>
 	`,
 	styles: [`
 		.completed { text-decoration: line-through; }
@@ -49,7 +51,11 @@ export class TodoListComponent implements OnInit {
 	
 	toggleTodo(todo: Todo) {
 		let newTodo = new Todo(todo.item, !todo.completed);
-		this._todoService.saveTodo(todo, newTodo);
+		this._todoService.saveTodo(todo, newTodo).then(
+			todos => {
+				this.todos = todos;
+			}			
+		);
 	}
 	
 	getTodos() {
@@ -65,19 +71,31 @@ export class TodoListComponent implements OnInit {
 	}
 	
 	saveEditedTodo(newTodo: Todo) {
-		console.log('_originTodo : ' + this._originTodo.item);
-		this._todoService.saveTodo(this._originTodo, newTodo).then(
-			todos => {
-				this.todos = todos;
-				this._todoOnEdit = null;
-			}			
-		);
+		this._todoOnEdit = null;
+		// check whether the todo item is modified
+		if(newTodo.item !== this._originTodo.item){
+			this._todoService.saveTodo(this._originTodo, newTodo).then(
+				todos => {
+					this.todos = todos;
+					
+				}			
+			);
+		}
 	}
 	
-	onEditTodo(todo: Todo) {
+	onEditTodo(todo: Todo, todoInput: HTMLInputElement) {
+		// todoInput.focus() doesn't work here, because the DOM hasn't been updated yet		
+		// In order to execute focus logic after the DOM has been updated, a timeout is used to push it onto a future VM turn.		
+		setTimeout(() => { todoInput.focus(); }, 0);
 		this._todoOnEdit = todo;
-		// cannot pass todo by reference!
+		// save origin todo item, cannot pass todo by reference!
 		this._originTodo = new Todo(todo.item, todo.completed);
+	}
+	
+	onCancelEdit(todo: Todo) {
+		this._todoOnEdit = null;
+		let index = this.todos.map(todo => todo.item).indexOf(todo.item);
+		this.todos[index] = this._originTodo;
 	}
 	
 	isEditing(todo: Todo) {
